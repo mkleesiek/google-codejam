8684709c0de75fc2c08ec567f10ce7421e4083b2
Marco Kleesiek <mkleesiek@cinemo.com>
2019-04-09 10:20:55 +0200

    Preparing gtest setup.

--- 2019/Qualification/C-test.cpp
+++ 2019/Qualification/C-test.cpp
@@ -1,12 +1,19 @@
 #include <gtest/gtest.h>
 
-#include "C.h"
+#define GTEST
+
+#include "C.cpp"
 
 using namespace std;
 
 TEST( GreatestCommonDivisor, Basic )
 {
-    ASSERT_EQ( gcd(12, 8), 4 );
-    ASSERT_EQ( gcd(8, 12), 4 );
-    ASSERT_EQ( gcd(8, 8), 8 );
+    ASSERT_EQ( __gcd(12, 8), 4 );
+    ASSERT_EQ( __gcd(8, 12), 4 );
+    ASSERT_EQ( __gcd(8, 8), 8 );
+}
+
+TEST( Solve, Basic )
+{
+
 }
--- 2019/Qualification/C.cpp
+++ 2019/Qualification/C.cpp
@@ -1,5 +1,3 @@
-#include "C.h"
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -10,6 +8,37 @@
 
 using namespace std;
 
+template<typename T>
+string solve(const vector<T>& C)
+{
+    T D = __gcd(C[0], C[1]);
+    vector<T> P(C.size()+1, 0);
+    P[0] = C[0] / D;
+    P[1] = D;
+    
+    for (size_t j = 2; j < P.size(); j++)
+    {
+        P[j] = C[j-1] / P[j-1];
+    }
+
+    set<T> primes(P.begin(), P.end());
+    unordered_map<T, char> pmap;
+
+    char c = 'A';
+    for (auto p : primes)
+    {
+        pmap[p] = c++;
+    }
+
+    string result(P.size(), ' ');
+    for (size_t i = 0; i < P.size(); i++)
+        result[i] = pmap[P[i]];
+
+    return result;
+}
+
+#ifndef GTEST
+
 int main(int argc, char* argv[])
 {
     ifstream in;
@@ -19,6 +48,15 @@ int main(int argc, char* argv[])
         in =  ifstream(inFile);
         cin.rdbuf(in.rdbuf());
     }
+
+#else
+
+int test(const string& arg)
+{
+    istringstream in(arg);
+    cin.rdbuf(in.rdbuf());
+
+#endif // GTEST
 
     int T = 0;
     cin >> T;
@@ -32,35 +70,13 @@ int main(int argc, char* argv[])
         vector<int> C(L);
 
         for (int i = 0; i < L; i++)
-        {
             cin >> C[i];
-        }
 
         cout << "Case #" << (t+1) << ": ";
 
-        int D = __gcd(C[0], C[1]);
-        vector<int> P(L+1, 0);
-        P[0] = C[0] / D;
-        P[1] = D;
-        
-        for (size_t j = 2; j < P.size(); j++)
-        {
-            P[j] = C[j-1] / P[j-1];
-        }
+        string solution = solve(C);
 
-        set<int> primes(P.begin(), P.end());
-        unordered_map<int, char> pmap;
-
-        char c = 'A';
-        for (auto p : primes)
-        {
-            pmap[p] = c++;
-        }
-
-        for (auto p : P)
-            cout << pmap[p];
-
-        cout << endl;
+        cout << solution << endl;
     }
 
     return 0;
