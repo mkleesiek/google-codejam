2c40858e57244564e6b7fd3d317f8dc06a8e9c17
Marco Kleesiek <marco.kleesiek@gmail.com>
2019-08-09 23:53:47 +0200

    Templates for Round 3.

---
+++ 2019/Round3/A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int T = 0;
+    cin >> T;
+
+    for (int t = 0; t < T; ++t)
+    {
+        cout << "Case #" << (t+1) << ": ";
+
+        cout << endl;
+    }
+
+    return 0;
+}
